191b78d2093ebbf9543474e768aca877df060e5d
Thierry Brethes <tbrethes@gmail.com>
2026-03-06 07:49:41 +1100

    Update JobExtension.js

--- UpviseJS Samples/Web App Extension/JobExtension.js
+++ UpviseJS Samples/Web App Extension/JobExtension.js
@@ -14,68 +14,15 @@
  * limitations under the License.
  */
 
-//////////////////////////////////
-// Add one persistant left pane item to the jobs appid      
-var LeftPaneJobExtension = class {
+class JobExtension1 extends Extension {
+
     static {
-        Extension.addLeftPane(this, "jobs");
-    }
-
-    // NEEDED BY Upvise Extension framework
-    static style(id) {
-        return "label:My Extension;img:news;";
-    }
-
-    // NEEDED BY Upvise Extension framework
-    static async run(id) {
-        // id is always null for left pane extension
-        alert("Left Pane Jpb Extension");
-    }
-}
-
-//////////////////////////////////
-// Add one toolbar button in the Jobs.viewJob() screen
-var ViewJobToolbarExtension = class {
-    static {
-      Extension.addToolbar(this,  "jobs", "Jobs.viewJob");
+        super.register();
     }
     
     static style(id) {
         // Note if you want to show the toolbar icon only for some job ids, you can use the id variable and return "" not to show the icon
-        return "img:news;label:My Extension";
-    }
-
-    static async run(id) {
-        alert("jobid: " + id);
-    }
-}
-
-//////////////////////////////////
-// Add one menu item in more popup menu in the Jobs.viewJob() screen
-var ViewJobToolbarMoreExtension = class {
-    static {
-      Extension.addToolbar(this, "jobs", "Jobs.viewJob");
-    }
-    
-    static style(id) {
-        // use more:1 to add the extension to the more oppu menu
-        return "more:1;img:news;color:blue;label:My Extension";
-    }
-
-    static async run(id) {
-        alert("jobid: " + id);
-    }
-}
-
-//////////////////////////////////
-// Add one button in the Jobs.viewJob() screen for the appid: jobs app
-var ViewJobButtonExtension = class {
-    static {
-      Extension.addButton(this,  "jobs", "Jobs.viewJob");
-    }
-    
-    static style(id) {
-        return "img:news;backcolor:orange;compact:1;label:My Extension";
+        return "img:news;color:bluelabel:My Extension";
     }
 
     static async run(id) {
